94bb23ec45665ee189c4ed2cfd8d8742888a3c34
Nildson <nildsonkleyton@hotmail.com>
2021-05-09 12:21:05 -0300

    Nova pasta para finaliazar o projeto

---
+++ DesafioMVFinalizando/src/main/java/util/TesteConexaoDao.java
@@ -0,0 +1,35 @@
+package util;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityTransaction;
+
+import dao.GeralDao;
+import dao.GeralDaoImpl;
+import entidade.CafeManha;
+import entidade.Colaborador;
+
+public class TesteConexaoDao {
+
+	public static void main(String[] args) {
+
+		GeralDao dao = new GeralDaoImpl();
+		Colaborador cb = new Colaborador();
+		CafeManha cf =new CafeManha();
+		CafeManha cf1 =new CafeManha();
+		
+		cb.setCpf("12345678903");
+		cb.setNome("teste3");
+		
+		cf.setItemCafe("Suco abacaxi");
+		cf.setColaborador(cb);
+		
+		cf1.setItemCafe("queijo");
+		cf1.setColaborador(cb);
+		
+		dao.inserir(cb);
+		dao.inserir(cf);
+		dao.inserir(cf1);
+		
+	}
+
+}
